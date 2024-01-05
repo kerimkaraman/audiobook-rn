0fa00e6eaac2692b24928e37a826cae9a727e8b2
Kerim Karaman <55785875+kerimkaraman@users.noreply.github.com>
2024-01-05 21:17:01 +0300

    func:changes on millisecond converter

--- millisecondsConverter.js
+++ millisecondsConverter.js
@@ -1,12 +1,8 @@
 export function convertMillisecondsToTime(ms) {
-  // 1 saniye = 1000 milisaniye
-  // 1 dakika = 60 saniye
-  // 1 saat = 60 dakika
   const seconds = Math.floor((ms / 1000) % 60);
   const minutes = Math.floor((ms / (1000 * 60)) % 60);
   const hours = Math.floor((ms / (1000 * 60 * 60)) % 24);
 
-  // Zamanı iki haneli sayılara dönüştür
   const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
   const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
   const formattedHours = hours < 10 ? `0${hours}` : hours;
